1bbc9a243bcdb0390eed7c0486f952a6dbebad3c
starberdyev <starberdyev@mail.ru>
2020-11-27 13:19:40 +0300

    Add Program

--- Betting/Data/BettingContext.cs
+++ Betting/Data/BettingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Betting.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Betting
 {
--- Betting/Program.cs
+++ Betting/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Betting.Data.Models;
+using System;
 
 namespace Betting
 {
@@ -6,7 +7,63 @@ namespace Betting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new BettingContext())
+            {
+                var user = new User();
+                user.Name = "Bob";
+                user.Username = "Bobina";
+                user.Balance = 77777;
+                user.Email = "somebody@mail.ru";
+
+                context.Users.Add(user);
+
+                var town = new Town();
+                town.Name = "Rome";
+
+                context.Towns.Add(town);
+
+                var team = new Team();
+                team.Name = "Lacio";
+                team.Budget = 1000000;
+                team.Initials = "LAC";
+
+                context.Teams.Add(team);
+
+                var position = new Position();
+                position.Name = "attack";
+
+                context.Positions.Add(position);
+
+                var player = new Player();
+                player.Name = "Chiro";
+                player.IsInjured = false;
+                player.SquadNumber = 27;
+
+                context.Players.Add(player);
+
+                var country = new Country();
+                country.Name = "Italy";
+
+                context.Countries.Add(country);
+
+                var game = new Game();
+                game.AwayTeamBetRate = 3;
+                game.HomeTeamBetRate = 2;
+                game.DrawBetRate = 4;
+                game.AwayTeamGoals = 7;
+                game.HomeTeamGoals = 17;
+                game.DateTime = DateTime.Today;
+
+                context.Games.Add(game);
+
+                var playerStatistic = new PlayerStatistic();
+                playerStatistic.Assist = 5;
+                playerStatistic.MinutesPlayed = 179;
+                playerStatistic.ScoredGoals = 9;
+
+                context.PlayerStatistics.Add(playerStatistic);
+
+            }
         }
     }
 }
